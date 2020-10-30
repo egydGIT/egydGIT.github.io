f22ef60733ebf35e35bc5c646525181e05c89224
Eszter <egyd.eszter@gmail.com>
2020-10-30 15:28:29 +0100

    GitHub link

---
+++ js/JAVAim.js
@@ -0,0 +1,42 @@
+let Button = document.querySelector("#calcCredit");
+Button.onclick = function() {
+    alert("JAVAsoljuk, folyamatosan tanuljanak.");
+};
+
+// A felvételi harmadik szakaszában a álhírek felismerő képességüket, valamint a jogi kompetenciáikat teszteltük. Gratulálunk minden kedves - továbbiakban, mint - Résztvevőnek!   
+
+/*
+// under CONSTRiCTION
+
+
+// Alert:
+let alertCloseButtons = document.querySelectorAll(".close");
+let alertCloseEventHandlerFunction = function(ev) {
+    this.parentElement.style.display = "none";
+};
+for (let i=0; i < alertCloseButtons.length; i++) {
+    alertCloseButtons[i].addEventListener("click", alertCloseEventHandlerFunction);
+}
+
+
+// Credit calculation
+let gotPlusScore = function() {
+    let score = 1;
+    let scoreField = document.querySelector("#scoreField");
+    let AllScore = scoreField.value;
+    
+    AllScorePlusOne = AllScore + score; 
+    scoreField.innerHTML = AllScorePlusOne;
+}
+
+
+// OTP Balance
+let Ballance = document.querySelector("#Ballance")
+let numericArray = [2500000, 2400000, 2300000, 2200000, 2100000, 2000000, 
+    1900000, 1800000, 1700000, 1600000, 1500000, 1400000, 1300000, 1200000, 1100000, 1000000, 
+    900000, 800000, 700000, 600000, 500000, 400000, 300000, 200000, 100000, 0];
+for (let i = 0; i < numericArray.length; i++) {
+    // sconsole.log(numericArray[i]);
+    Ballance.innerHTML = numericArray[i];
+}
+*/
---
+++ js/onClick.js
@@ -0,0 +1,5 @@
+// Hogy kell másik html-re mutatni gombon ??? 
+let Buttun2.1 = document.querySelector("#2.1");
+Buttun2.1.onclick = function() {
+    href="/pages/index.html";
+};
